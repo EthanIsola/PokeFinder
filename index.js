b610fc80a51d742f946332778d1669de2f11b3ca
Ethan Isola <isolaethan@gmail.com>
2022-01-24 10:09:40 -0600

    added pokesearch function and DOMContentLoaded event listener

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-hello
+let nameSearch = "https://pokeapi.co/api/v2/pokemon/"
+let name;
+
+addEventListener('DOMContentLoaded', ()=> {
+    fetch(`${nameSearch}${name}`)
+    .then(promise => promise.json)
+    .then(pokemon => {
+
+    })
+})
+
+
+//parses data from pokemon search fetch request
+function pokeSearch(pokeList){
+    for(let poke of pokeList){
+        if (poke = "name"){}
+        else if(poke = "height"){}
+        else if(poke = "weight"){}
+        else if(poke = "types"){
+            for(let item of poke){
+                if(item = "type"){}
+            }
+        }
+    }
+}
+
